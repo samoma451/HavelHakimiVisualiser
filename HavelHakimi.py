c7977f00f0006651e1aa81f1f197810c420749a1
samoma451 <19236719@studentmail.ul.ie>
2022-09-18 23:09:08 +0100

    Added graphing capabilities. Still needs some testing

--- HavelHakimi.py
+++ HavelHakimi.py
@@ -1,36 +1,49 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+
 def run_algorithm(dSeq):
     degreeRecord = []
-    for i in range(0, len(dSeq)-1):
+    for i in range(0, len(dSeq) - 1):
         dSeq.sort(reverse=True)
         degreeRecord.append(list(dSeq))
         print(dSeq)
         count = dSeq[0]
-        print(count)
         if count == 0:
             break
         dSeq.pop(0)
         if len(dSeq) < count:
             break
-        for j in range(0,count):
+        for j in range(0, count):
             dSeq[j] = dSeq[j] - 1
-            print(dSeq)
 
     if dSeq[0] == 0:
-        print("graph valid")
+        print("graph exists")
         create_graph(degreeRecord)
     else:
-        print("graph invalid")
+        print("graph does not exist")
 
-def create_graph(degreeRecord, seed = 2022):
+
+def create_graph(degreeRecord):
     G = nx.Graph()
+    graphs = []
     for i in range(1, len(degreeRecord[-1]) + 1):
         G.add_node(i)
-        #print(G.nodes())
-    nx.draw(G)
+    graphs.append(G.copy())
+
+    numSteps = len(degreeRecord[0]) - len(degreeRecord[-1])
+    for j in range(0, numSteps):
+        G.add_node(j + 2)
+        newNodeDegree = degreeRecord[-2 - j][0]
+        for k in range(0, newNodeDegree):
+            G.add_edge(j + 2, k)
+        graphs.append(G.copy())
+
+    for g in range(0, len(graphs)):
+        plt.figure(g + 1)
+        nx.draw(graphs[-g-1])
     plt.show()
 
-testSeq = [4,3,4,3,2]
+
+testSeq = [4, 3, 4, 3, 2]
 run_algorithm(testSeq)
