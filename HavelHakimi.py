05f7a72947571603472b9751de1ed052c0370242
samoma451 <19236719@studentmail.ul.ie>
2022-09-19 12:08:42 +0100

    Fixed issue with nodes changing label from {1,2,...} to {0,1,2,...} when generating 2nd graph onwards. Added attempt to keep relative layout of nodes but has not done as intended, requires further update

--- HavelHakimi.py
+++ HavelHakimi.py
@@ -32,16 +32,17 @@ def create_graph(degreeRecord):
     graphs.append(G.copy())
 
     numSteps = len(degreeRecord[0]) - len(degreeRecord[-1])
-    for j in range(0, numSteps):
+    for j in range(1, numSteps+1):
         G.add_node(j + 2)
-        newNodeDegree = degreeRecord[-2 - j][0]
+        newNodeDegree = degreeRecord[-1 - j][0]
         for k in range(0, newNodeDegree):
-            G.add_edge(j + 2, k)
+            G.add_edge(j + 2, k+1)
         graphs.append(G.copy())
 
     for g in range(0, len(graphs)):
         plt.figure(g + 1)
-        nx.draw(graphs[-g-1])
+        nodePos = nx.spring_layout(graphs[-g-1])
+        nx.draw(graphs[-g-1],with_labels=True,pos=nodePos)
     plt.show()
 
 
