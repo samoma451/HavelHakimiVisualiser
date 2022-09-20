aac8c570f6963da4c0db861f4c7c492cabf6f79d
samoma451 <samoma451@gmail.com>
2022-09-20 18:12:50 +0100

    Added create_new_window() function to inform user if the degree sequence was determined to be graphical or not. Began adding comments for readability

--- HavelHakimi.py
+++ HavelHakimi.py
@@ -1,8 +1,16 @@
-import networkx as nx
-import matplotlib.pyplot as plt
+from tkinter import *  # tkinter used to make new windows
+import networkx as nx  # library used for creating and drawing graphs
+import matplotlib.pyplot as plt  # used for plotting our graph, could be used to change axes, include subplots, etc.
 
 
 def run_algorithm(dSeq):
+    """ This function runs the Havel-Hakimi algorithm, which determines if a degree sequence is graphical.
+    If the sequence is graphical, then the create_graph() function is called. Otherwise a new window informs
+    the user that the sequence is not graphical.
+
+    :param dSeq The degree sequence of the graph to run the algorithm on:
+    :return:
+    """
     degreeRecord = []
     for i in range(0, len(dSeq) - 1):
         dSeq.sort(reverse=True)
@@ -18,10 +26,10 @@ def run_algorithm(dSeq):
             dSeq[j] = dSeq[j] - 1
 
     if dSeq[0] == 0:
-        print("graph exists")
+        create_new_window("Congratulations", degreeRecord, TRUE)
         create_graph(degreeRecord)
     else:
-        print("graph does not exist")
+        create_new_window("Sorry My Friend", degreeRecord, FALSE)
 
 
 def create_graph(degreeRecord):
@@ -32,19 +40,36 @@ def create_graph(degreeRecord):
     graphs.append(G.copy())
 
     numSteps = len(degreeRecord[0]) - len(degreeRecord[-1])
-    for j in range(1, numSteps+1):
+    for j in range(1, numSteps + 1):
         G.add_node(j + 2)
         newNodeDegree = degreeRecord[-1 - j][0]
         for k in range(0, newNodeDegree):
-            G.add_edge(j + 2, k+1)
+            G.add_edge(j + 2, k + 1)
         graphs.append(G.copy())
 
     for g in range(0, len(graphs)):
         plt.figure(g + 1)
-        nodePos = nx.spring_layout(graphs[-g-1])#issue with layout here, how to keep consist node positioning between graphs?
-        nx.draw(graphs[-g-1],with_labels=True,pos=nodePos)
+        nodePos = nx.spring_layout(
+            graphs[-g - 1])  # issue with layout here, how to keep consist node positioning between graphs?
+        nx.draw(graphs[-g - 1], with_labels=True, pos=nodePos)
     plt.show()
 
 
-testSeq = [4, 3, 4, 3, 2]
+def create_new_window(titleText, degreeRecord, isGraphical):
+    win = Tk()
+    win.title(titleText)
+    win.geometry('500x200+50+50')
+
+    if isGraphical:
+        labelText = "Degree Sequence: " + str(degreeRecord[0]) + "\nThat degree sequence is graphical" + \
+                    "\nPlease close this window to see how such a graph can be constructed"
+    else:
+        labelText = "Degree Sequence: " + str(degreeRecord[0]) + "\nThat degree sequence is not graphical"
+
+    tkinter.Label(text=labelText, height=50, font=("Calibri", 12)).pack()
+
+    win.mainloop()
+
+
+testSeq = [6, 6, 6, 6, 6, 6, 6, 6]
 run_algorithm(testSeq)
